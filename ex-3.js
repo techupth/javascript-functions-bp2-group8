9ebd982b8784f3c78ba37c99d94dff63149474ac
fonytas <fonyfonyta@gmail.com>
2023-11-21 14:00:27 +0700

    Feat Assignments:Functions:Finished Assignments

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,14 @@
 // Start coding here
+//exercise 3
+let addFunction = (a,b) => a+b;;
+console.log(addFunction(10,5));
+
+let substractFunction = (a,b) => a-b;
+;
+console.log(substractFunction(10,5));
+
+let multiplyFunction = (a,b) => a*b;
+console.log(multiplyFunction(10,5));
+
+let divideFunction = (a,b) => a/b;
+console.log(divideFunction(10,5));
